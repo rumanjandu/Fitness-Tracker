e4e8a0f27199dd07dbf0d877e2c89dc6eff0dc37
rumanjandu <79531428+rumanjandu@users.noreply.github.com>
2023-03-06 18:17:56 +0000

    changes to file names and creation of registerScreen.java

--- app/src/main/java/com/example/fitnesstracker/registerScreen.java
+++ app/src/main/java/com/example/fitnesstracker/registerScreen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class loginScreen extends AppCompatActivity {
+public class registerScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_register_screen);
     }
 }
