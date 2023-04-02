ec61ec6647dc32c24401c9bead32c5933810136a
rumanjandu <79531428+rumanjandu@users.noreply.github.com>
2023-04-02 21:40:06 +0100

    sign up goal new activity

--- app/src/main/java/com/example/fitnesstracker/registerScreenGoal.java
+++ app/src/main/java/com/example/fitnesstracker/registerScreenGoal.java
@@ -2,7 +2,12 @@ package com.example.fitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class registerScreenGoal extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class registerScreenGoal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen_goal);
+    }
+
+    public void spinnerMeaurementSelection() {
+        Spinner spinnerMeasurements = (Spinner) findViewById(R.id.spinnerMeasurementSystem);
+        String stringMeasurementSystem = spinnerMeasurements.getSelectedItem().toString();
+        TextView textViewMeasurements = (TextView) findViewById(R.id.textViewMeasurementSystem);
+        Spinner spinnerWeeklyGoal = (Spinner) findViewById(R.id.spinnerWeeklyGoal);
+
+        if (stringMeasurementSystem.equals("Metric")) {
+            //do nothing
+
+        } else if (stringMeasurementSystem.equals("Imperial")) {
+            //change spinner entries to different array
+
+        } else {
+
+        }
     }
 }
